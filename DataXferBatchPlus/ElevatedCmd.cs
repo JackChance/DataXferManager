ca3b285729a4fa9627a3d60216615e3d80a17996
JackChance <strakacameron@hotmail.com>
2014-12-15 12:49:34 -0800

    Patch to fix 'unhide' from running when no folder was created, which was throwing an error in command prompt when test only was selected and a nonexistant target chosen.
    More error resistant, nicer UI and no more scaring the user.

--- DataXferBatchPlus/ElevatedCmd.cs
+++ DataXferBatchPlus/ElevatedCmd.cs
@@ -11,7 +11,44 @@ namespace DataXferBatchPlus
     {
         public bool runElevatedCmd(string command1, string command2)
         {
-            command1 = "/S /K " + command1+ " & " +command2;
+            command1 = "/S /K " + command1;
+            //check to see if this is just a test, i.e. has the string '/q '
+            bool skipcom2a = false;
+            bool skipcom2b = false;
+            string c;
+            foreach(char b in command1)
+            {
+                c = b.ToString();
+                //stop checking if they are both true.
+                if(!skipcom2b)
+                {
+                if (c == "/" && skipcom2a ==false)
+                {
+                    skipcom2a = true;
+                }else 
+                    if(c == "q" && skipcom2a == true)
+                    {
+                        skipcom2b = true;
+                    }else
+                        if(c == " " && skipcom2b == true)
+                        {
+                            
+                        }
+                        else
+                        {
+                            skipcom2a = false;
+                            skipcom2b = false;
+                        }
+                }
+            }
+            if(!skipcom2a || !skipcom2b)
+            {
+               
+            }
+            else
+            {
+                command1 += " & " + command2;
+            }
             ProcessStartInfo mystart = new ProcessStartInfo("cmd.exe");
             mystart.Verb = "runas";
             mystart.Arguments = command1;
