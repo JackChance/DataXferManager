f17670ee8ff0b0aa40cefa2901404216ad5d1c16
JackChance <strakacameron@hotmail.com>
2014-12-20 02:54:02 -0800

    Version 1.0.0.3
    Corrected bugs related to un hiding documents; The reverse of anticipated behavoir was occuring.

--- DataXferBatchPlus/ElevatedCmd.cs
+++ DataXferBatchPlus/ElevatedCmd.cs
@@ -11,7 +11,7 @@ namespace DataXferBatchPlus
     {
         public bool runElevatedCmd(string command1, string command2)
         {
-            command1 = "/S /K " + command1;
+            command1 = "echo /S /K " + command1;
             //check to see if this is just a test, i.e. has the string '/q '
             bool skipcom2a = false;
             bool skipcom2b = false;
@@ -20,33 +20,33 @@ namespace DataXferBatchPlus
             {
                 c = b.ToString();
                 //stop checking if they are both true.
-                if(!skipcom2b)
+                if(!skipcom2b || !skipcom2a)
                 {
-                if (c == "/" && skipcom2a ==false)
-                {
-                    skipcom2a = true;
-                }else 
-                    if(c == "q" && skipcom2a == true)
+                    if (c == "/")
                     {
+                        //if this is a / then set hook one to true
+                        skipcom2a = true;
+                    } else
+                    if(c == "l" && skipcom2a)
+                    {
+                        //if hook one is true and this is l then set hook two to true ("/l" is the switch for test copy.)
                         skipcom2b = true;
-                    }else
-                        if(c == " " && skipcom2b == true)
-                        {
-                            
-                        }
-                        else
-                        {
-                            skipcom2a = false;
-                            skipcom2b = false;
-                        }
+                    }
+                    if(c != "/" && c !="l" && c!= " ")
+                    {
+                        // if this isn't a /, l or space then reset both hooks to false, it means the string was "/l"+x, where x is anything invalid for the "/l" switch
+                        skipcom2a = false;
+                        skipcom2b = false;
+                    }
                 }
             }
-            if(!skipcom2a || !skipcom2b)
+            if(skipcom2a && skipcom2b)
             {
-               
+               //do nothing if both hooks are true
             }
             else
             {
+                //If both aren't true then remove the system and hidden tags from the result folder, just in case.
                 command1 += " & " + command2;
             }
             ProcessStartInfo mystart = new ProcessStartInfo("cmd.exe");
